ccf206774583cd87c0be82caf7884163c1e081a1
derek <derekchili@gmail.com>
2023-05-27 11:30:29 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
 import React from 'react';
 import Header from './components/common/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-import './App.css';
+import Footer from './components/common/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <h1>Hello</h1>
         <AboutMe />
         <Portfolio />
         <Contact />
---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+
+export default function AboutMe(){
+    return(
+        <>
+        <h1>Derek Chilson</h1>
+        <h2>I am trying to figure out how to code!</h2>
+        </>
+    )
+}
