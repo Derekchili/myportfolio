df07b8c096fcbb9774082446548b00208e60e770
derek <derekchili@gmail.com>
2023-05-31 06:52:58 -0700

    updated page not found

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,8 +1,8 @@
 export default function NotFound(){
     return(
-        <section>
-            <h1>404 Error!</h1>
-            <p>Sorry!! Page not found</p>
+        <section className="flex flex-col items-center">
+            <h1 className="text-red-500 border-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 hover:font-bold hover:bg-blue-400 text-xl font-medium hover:text-2xl">404 Error!</h1>
+            <h3 className="text-red-500 border-3 border-4 border-gray-700 rounded-lg shadow-lg bg-grey-300 hover:font-bold hover:bg-blue-400 text-xl font-medium hover:text-2xl">Sorry!! Page not found</h3>
         </section>
     );
 }
